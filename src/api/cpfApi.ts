8278c0beb70d508ee1276f931f815868c5554e3f
augustolliveiira <06augustogabriel@gmail.com>
2025-05-05 22:09:46 -0300

    Add files via upload

--- src/api/cpfApi.ts
+++ src/api/cpfApi.ts
@@ -1,7 +1,7 @@
 
 import { CpfValidationResponse } from '../types';
 
-const API_TOKEN = '95150b0b9cc3dcb0ae0b24a66514a8360cb293324fb65ffb76f783133018cfc8';
+const API_TOKEN = 'vaicoritnhinas95150b0b9cc3dcb0ae0b24a66514a8360cb293324fb65ffb76f783133018cfc8';
 
 export async function validateCpfApi(cpf: string): Promise<CpfValidationResponse> {
   try {
